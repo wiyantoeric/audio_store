3729df67760e7f6cd2a49f95ee3815e6c6d00260
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2024-01-10 19:29:47 +0700

    feat: create app router

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:audio_store/router.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -7,15 +8,16 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
       title: 'Audio Store',
       theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.amber),
         useMaterial3: true,
       ),
+      themeMode: ThemeMode.dark,
+      routerConfig: router,
     );
   }
 }
---
+++ lib/router.dart
@@ -0,0 +1,12 @@
+import 'package:audio_store/pages/home_page.dart';
+import 'package:go_router/go_router.dart';
+
+final router = GoRouter(
+  initialLocation: '/',
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => HomePage(),
+    ),
+  ],
+);
