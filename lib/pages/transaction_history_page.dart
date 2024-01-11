d77fe359b71dec5c3bd8b42040c3aebe655c7616
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2024-01-11 15:46:54 +0700

    feat: routed transaction page

---
+++ lib/pages/transaction_history_page.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class TransactionHistoryPage extends StatelessWidget {
+  const TransactionHistoryPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
--- lib/router.dart
+++ lib/router.dart
@@ -1,6 +1,7 @@
 import 'package:audio_store/pages/home_page.dart';
 import 'package:audio_store/pages/login_page.dart';
 import 'package:audio_store/pages/register_page.dart';
+import 'package:audio_store/pages/transaction_history_page.dart';
 import 'package:go_router/go_router.dart';
 
 final router = GoRouter(
@@ -21,6 +22,10 @@ final router = GoRouter(
     GoRoute(
       path: '/settings',
       builder: (context, state) => RegisterPage(),
+    ),
+    GoRoute(
+      path: '/transactions',
+      builder: (context, state) => TransactionHistoryPage(),
     ),
   ],
 );
