3caee73327e5f05c43c43c4e49269d64794cee18
Eric Wiyanto <77096303+wiyantoeric@users.noreply.github.com>
2024-01-13 15:32:35 +0700

    feat: transaction page

--- lib/pages/transactions_page.dart
+++ lib/pages/transactions_page.dart
@@ -1,7 +1,9 @@
 import 'package:audio_store/database/supabase_controller.dart';
 import 'package:audio_store/model/cart_item.dart';
 import 'package:audio_store/model/transaction.dart';
+import 'package:audio_store/widgets/transaction_card.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:supabase_flutter/supabase_flutter.dart';
 import 'package:logger/logger.dart';
 
@@ -22,36 +24,63 @@ class _TransactionsPageState extends State<TransactionsPage> {
           child: SizedBox(
             width: double.infinity,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                ElevatedButton(
-                    onPressed: () {
-                      getTransactions(
-                          uid: Supabase.instance.client.auth.currentUser!.id);
-                    },
-                    child: Text('Refresh')),
+                IconButton(
+                  onPressed: () {
+                    context.go('/');
+                  },
+                  icon: const Icon(Icons.arrow_back),
+                ),
                 Expanded(
-                  child: FutureBuilder(
-                      future: getTransactions(
-                          uid: Supabase.instance.client.auth.currentUser!.id),
-                      builder: (context, snapshot) {
-                        if (snapshot.hasData) {
-                          final transactions = snapshot.data as List;
-                          final itemIds = [];
-                          for (Transaction t in transactions) {
-                            itemIds.addAll(t.itemIds);
-                          }
+                  child: Supabase.instance.client.auth.currentSession == null
+                      ? Column(
+                          children: [
+                            FilledButton(
+                              onPressed: () => context.go('/login'),
+                              child: Text('Login'),
+                            ),
+                            Text('Please login to view transactions'),
+                          ],
+                        )
+                      : FutureBuilder(
+                          future: getTransactions(
+                              uid: Supabase
+                                  .instance.client.auth.currentUser!.id),
+                          builder: (context, snapshot) {
+                            if (snapshot.hasData) {
+                              final transactions = snapshot.data as List;
+                              final itemIds = [];
+                              for (Transaction t in transactions) {
+                                itemIds.addAll(t.itemIds);
+                              }
 
-                          return ListView.builder(
-                              itemCount: transactions.length,
-                              itemBuilder: (context, index) {
-                                return;
-                              });
-                        } else {
-                          return const Center(
-                            child: CircularProgressIndicator(),
-                          );
-                        }
-                      }),
+                              return ListView.separated(
+                                  itemCount: transactions.length,
+                                  separatorBuilder: (context, index) {
+                                    return Column(
+                                      children: [
+                                        SizedBox(height: 8),
+                                        Divider(
+                                            color: Theme.of(context)
+                                                .colorScheme
+                                                .outline),
+                                        SizedBox(height: 8),
+                                      ],
+                                    );
+                                  },
+                                  itemBuilder: (context, index) {
+                                    return TransactionCard(
+                                      transaction: transactions[index],
+                                    );
+                                  });
+                            } else {
+                              return const Center(
+                                child: CircularProgressIndicator(),
+                              );
+                            }
+                          },
+                        ),
                 ),
               ],
             ),
--- lib/widgets/transaction_card.dart
+++ lib/widgets/transaction_card.dart
@@ -1,14 +1,136 @@
+import 'package:audio_store/database/supabase_controller.dart';
+import 'package:audio_store/model/item.dart';
 import 'package:audio_store/model/transaction.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 
-class TransactionCard extends StatelessWidget {
+import 'package:logger/logger.dart';
+
+class TransactionCard extends StatefulWidget {
   const TransactionCard({super.key, required this.transaction});
   final Transaction transaction;
 
   @override
+  State<TransactionCard> createState() => _TransactionCardState();
+}
+
+class _TransactionCardState extends State<TransactionCard> {
+  late String orderDate;
+
+  @override
+  void initState() {
+    DateTime dateTime = DateTime.parse(widget.transaction.createdAt!);
+    DateFormat outputFormatter = DateFormat("dd MMM yyyy");
+    orderDate = outputFormatter.format(dateTime);
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Container(
-      child: Text(transaction.itemIds.toString()),
+      padding: const EdgeInsets.all(16),
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(24),
+        border: Border.all(color: Theme.of(context).colorScheme.outline),
+      ),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(orderDate),
+          SizedBox(height: 16),
+          ...widget.transaction.itemIds.asMap().entries.map(
+            (e) {
+              int idx = e.key;
+              int itemId = e.value;
+              int qty = widget.transaction.qtys[idx];
+
+              return FutureBuilder<Item?>(
+                future: getItem(id: itemId),
+                builder: (context, snapshot) {
+                  if (snapshot.connectionState == ConnectionState.done) {
+                    final item = snapshot.data as Item;
+
+                    return Column(
+                      children: [
+                        Row(
+                          children: [
+                            Expanded(
+                              flex: 2,
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(24),
+                                child: Image.network(
+                                  item.imageUrl!,
+                                ),
+                              ),
+                            ),
+                            SizedBox(width: 16),
+                            Expanded(
+                              flex: 3,
+                              child: Column(
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: [
+                                  Text(
+                                    item.name,
+                                    style:
+                                        Theme.of(context).textTheme.titleMedium,
+                                  ),
+                                  SizedBox(height: 8),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text('Price',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelLarge),
+                                      Text('Qty',
+                                          style: Theme.of(context)
+                                              .textTheme
+                                              .labelLarge),
+                                    ],
+                                  ),
+                                  Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      Text(
+                                        'USD${item.price.toString()}',
+                                      ),
+                                      Text(qty.toString()),
+                                    ],
+                                  ),
+                                  SizedBox(height: 8),
+                                  Text(
+                                    'Subtotal : USD${qty * item.price}',
+                                    style:
+                                        Theme.of(context).textTheme.titleMedium,
+                                  ),
+                                ],
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 16),
+                      ],
+                    );
+                  } else if (snapshot.hasError) {
+                    Logger().e(snapshot.error);
+                    return Text('Error occured');
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+              );
+            },
+          ).toList(),
+          Text(
+            'Total : USD${widget.transaction.totalPrice}',
+            style: Theme.of(context).textTheme.titleMedium,
+          ),
+        ],
+      ),
     );
   }
 }
